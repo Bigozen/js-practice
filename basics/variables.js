be52b25721892de5af0782bc0775ad310d415f56
Bigozen <114302215+Bigozen@users.noreply.github.com>
2025-04-26 17:28:58 +0000

    variables file added

---
+++ basics/variables.js
@@ -0,0 +1,9 @@
+const accountId= 14425 //can't change
+let accountEmail="tabassum@gmail.com" //best to use 
+var accountType="saving" //not to use var because of scope
+accountAdress="Mirpur"
+let accountState;
+
+console.log("Variables")
+console.table([accountId,accountEmail,accountType,accountAdress,accountState])
+
